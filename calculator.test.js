2bb5e9de4e19dcedb72f461a9daabd7d9f4c0b8a
alexoc123 <98850434+alexoc123@users.noreply.github.com>
2022-11-08 21:54:36 +0000

    Update calculator.js
    CI Workflow

--- calculator.test.js
+++ calculator.test.js
@@ -1,19 +1,8 @@
 var calculator = require('./calculator');
 test('Add numbers', ()=>{
-    expect(calculator.).toBe(1+1)
+    expect(1+1).toBe(2)
 })
 /* global expect, calculate */
 
 import calculate from './calculator';
-test('Add numbers', (1,4)=>{
-    expect().toBe(5);
-});
-test('multiply numbers',(5,3)=>{
-    expect().toBe(15);
-});
-test('divide numbers',(21,3)=>{
-    expect().toBe(7);
-});
-test('subtract numbers',(10,9)=>{
-    expect().toBe(1);
-});
+
