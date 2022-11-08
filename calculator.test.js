b8d31f0375b20a33122e332996688beaaf169991
DESKTOP-J2II23C\saipr <gaminis@tcd.ie>
2022-11-08 18:37:17 +0000

    test files

--- calculator.test.js
+++ calculator.test.js
@@ -1,4 +1,15 @@
-import { } from './calculator';
-test('Add numbers',()=>{
-    expect().toBe(1+1)
-})
+/* global expect, calculate */
+
+import calculate from './calculator';
+test('Add numbers',(1,4)=>{
+    expect().toBe(5);
+});
+test('multiply numbers',(5,3)=>{
+    expect().toBe(15);
+});
+test('divide numbers',(21,3)=>{
+    expect().toBe(7);
+});
+test('subtract numbers',(10,9)=>{
+    expect().toBe(1);
+});
