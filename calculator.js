a148edf4d11cd2bece2a8b11748e15be7f2b9004
DESKTOP-J2II23C\saipr <gaminis@tcd.ie>
2022-11-08 20:11:02 +0000

    no message

--- calculator.js
+++ calculator.js
@@ -1,13 +1,61 @@
-function clearResult(){
-    document.getElementById("result").value="";
+var a = '';
+var b = '';
+var num = [];
+var ans;
+
+// All the numbers and operators input will be stored in an array "num" using function "sendNum()"
+function sendNum(digit){
+
+	num.push(digit);
+
+	if(num.length !== 1){
+		a = '';
+		document.getElementById('screen').innerHTML = a;		// clearing the screen.
+	}
+
+
+	for(i=0; i<num.length ; i++){
+
+		a = a + num[i];				// concatenate the elements of the array "num" into a single string, which will be displayed on the screen
+		
+	}
+
+document.getElementById('screen').innerHTML = a;	// displaying the concatenated string
+
+	
 }
 
-function display(value){
-    document.getElementById("result").value += value;
+// when the user presses "=", function "equalTo()" is called 
+function equalTo(){
+	document.getElementById('screen').innerHTML = '';
+
+	for(i=0; i<num.length ; i++){
+
+		b += num[i];						// concatenating the array "num" into a single string
+	}
+
+	ans = eval(b);	
+
+	document.getElementById('screen').innerHTML = ans;		// result display
+
+	while(num.length > 0){
+    	num.pop();				// emptying the array "num"
+	}
+
+	num.push(ans.toString());
+
+	
 }
 
-function calculate(){
-    var expression = document.getElementById("result").value;
-    var answer = eval(expression);
-    document.getElementById("result").value=answer ;
+
+// When user presses "AC", function "clearScr()" is called
+function clearScr(){
+	document.getElementById('screen').innerHTML = '';
+	
+	while(num.length > 0){
+    	num.pop();				// emptying the array "num"
+	}
+
+	a ='';	
+	b ='';
 }
